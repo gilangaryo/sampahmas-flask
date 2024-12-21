b4cf1ab4e5a00eb01ac85d02693fafcf01fadeff
gilangaryo <gilang91668@gmail.com>
2024-12-21 17:36:42 +0700

    update main lagi

--- main.py
+++ main.py
@@ -19,11 +19,8 @@ app.config['MAX_CONTENT_LENGTH'] = 40 * 1024 * 1024
 
 
 
-# DEVELOPMENT
-if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\code\backend-sampahmas-deteksi\serviceAccount.json'
+service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS').strip()
 
-service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 if service_account_path is None:
     raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS not set")
 
@@ -31,16 +28,6 @@ cred = credentials.Certificate(service_account_path)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'sampahmas-3a4f0.appspot.com'
 })
-
-# service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS').strip()
-
-# if service_account_path is None:
-#     raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS not set")
-
-# cred = credentials.Certificate(service_account_path)
-# firebase_admin.initialize_app(cred, {
-#     'storageBucket': 'sampahmas-3a4f0.appspot.com'
-# })
 
 bucket = storage.bucket()
 
