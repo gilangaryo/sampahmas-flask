2d3e2081492086dd8bf8359b98e220560d05a9f7
gilangaryo <gilang91668@gmail.com>
2025-01-15 12:04:15 +0700

    fix sekali

--- main.py
+++ main.py
@@ -18,21 +18,10 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 40 * 1024 * 1024
 
 # DEVELOPMENT
-if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\code\backend-sampahmas-deteksi\serviceAccount.json'
+# if not os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
+#     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\code\backend-sampahmas-deteksi\serviceAccount.json'
 
-service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-if service_account_path is None:
-    raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS not set")
-
-cred = credentials.Certificate(service_account_path)
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'sampahmas-3a4f0.appspot.com'
-})
-
-# PRODUCTION
-# service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS').strip()
-
+# service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 # if service_account_path is None:
 #     raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS not set")
 
@@ -40,6 +29,17 @@ firebase_admin.initialize_app(cred, {
 # firebase_admin.initialize_app(cred, {
 #     'storageBucket': 'sampahmas-3a4f0.appspot.com'
 # })
+
+# PRODUCTION
+service_account_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS').strip()
+
+if service_account_path is None:
+    raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS not set")
+
+cred = credentials.Certificate(service_account_path)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'sampahmas-3a4f0.appspot.com'
+})
 
 bucket = storage.bucket()
 
@@ -65,33 +65,33 @@ def detect_bottle_and_draw(frame):
 
     inference_time = end_time - start_time
 
-    detected_objects = []
+    bottle_found = False
+    percentage = 0
 
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
-        if confidence > 0.01:  # Ambang batas confidence untuk menampilkan hasil
+        if confidence > 0.1:
             idx = int(detections[0, 0, i, 1])
-            label = CLASSES[idx]
-            percentage = int(confidence * 100)
+            if CLASSES[idx] == "bottle":
+                logging.info("Bottle detected!")
+                bottle_found = True
+                percentage = int(confidence * 100)
 
-            box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-            (startX, startY, endX, endY) = box.astype("int")
+                box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+                (startX, startY, endX, endY) = box.astype("int")
 
-            # Tambahkan objek yang terdeteksi ke daftar
-            detected_objects.append({"label": label, "confidence": percentage, "box": (startX, startY, endX, endY)})
-
-            # Gambar kotak dan label pada frame
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            text = f"{label}: {percentage}%"
-            y = startY - 8 if startY - 10 > 10 else startY + 10
-            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                label = f"{CLASSES[idx]}: {percentage}%"
+                y = startY - 8 if startY - 10 > 10 else startY + 10
+                cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                break
 
     total_end_time = time.time()
     total_time = total_end_time - total_start_time
     logging.info(f"Inference time: {inference_time:.4f} seconds")
     logging.info(f"Total time (preprocessing + inference + postprocessing): {total_time:.4f} seconds")
 
-    return detected_objects, frame, inference_time, total_time
+    return bottle_found, frame, percentage, inference_time, total_time
 
 
 def upload_to_firebase(file_path, firebase_path):
